2c49ba6a12b5ac8ddf0d7efe3c8d71cdaf3618f4
Alejandro Uribe <uriberalejandro+github@gmail.com>
2026-09-07 20:47:35 -0400

    Added seed.sql
    Set up for club_members

---
+++ seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO club_members (first_name, last_name, email, major, join_date)
+VALUES 
+    ('john', 'Smith', 'john.smith@university.edu', 'Physics', '2025-03-15'),
+    ('Liam', 'Martinez', 'liam.m@university.edu', 'Information Technology', '2025-02-01'),
+    ('Sophia', 'Chen', 'sophia.chen@university.edu', 'Data Science', '2025-02-20'),
+    ('Noah', 'Williams', 'noah.w@university.edu', 'Cybersecurity', '2025-03-01'),
+    ('Olivia', 'Brown', 'olivia.brown@university.edu', NULL, '2025-03-10');
+
+SELECT * FROM club_members;
